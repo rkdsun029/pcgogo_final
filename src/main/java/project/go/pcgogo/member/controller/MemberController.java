15591ab0fc1a22534cff1cbccf33ac132702f413
sanghun Do <tkdgns3158@naver.com>
2019-02-12 21:18:02 +0900

    servlet-context.xml수정

--- src/main/java/project/go/pcgogo/member/controller/MemberController.java
+++ src/main/java/project/go/pcgogo/member/controller/MemberController.java
@@ -1,5 +1,13 @@
 package project.go.pcgogo.member.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class MemberController {
 
+	@RequestMapping("/register.do")
+	public String register() {
+		return "member/register";
+	}
 }
