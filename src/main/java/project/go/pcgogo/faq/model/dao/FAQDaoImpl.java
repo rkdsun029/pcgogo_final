2916e49379415d97534cbef8300a4be794f67dc0
sanghun Do <tkdgns3158@naver.com>
2019-02-14 17:00:04 +0900

    no message

--- src/main/java/project/go/pcgogo/faq/model/dao/FAQDaoImpl.java
+++ src/main/java/project/go/pcgogo/faq/model/dao/FAQDaoImpl.java
@@ -1,5 +1,8 @@
 package project.go.pcgogo.faq.model.dao;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class FAQDaoImpl implements FAQDao {
 
 }
--- src/main/java/project/go/pcgogo/faq/model/service/FAQServiceImpl.java
+++ src/main/java/project/go/pcgogo/faq/model/service/FAQServiceImpl.java
@@ -1,5 +1,8 @@
 package project.go.pcgogo.faq.model.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class FAQServiceImpl implements FAQService {
 
 }
